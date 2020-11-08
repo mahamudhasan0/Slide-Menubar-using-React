6c1327e3f90a4fc4dcaf808f198dec18552f78b8
Mahamud Hasan <72403684+mahamudhasan0@users.noreply.github.com>
2020-11-08 14:51:37 +0600

    Mahamud Hasan

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import  SlideMenus  from './SlideMenu';
+
+
+
+function App() {
+ 
+  return (
+    <div className="App">
+    <SlideMenus/>
+      
+    </div>
+  );
+}
+
+export default App;
